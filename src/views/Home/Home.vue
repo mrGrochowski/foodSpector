d365ca2259fde252a29e6c99c1f261a1ccafeae9
Unknown <mateusz.grochowski1992@gmail.com>
2019-12-09 01:37:30 +0100

    add few thing like bootstrap 4 grid system & add first components

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <wellcome-jumbo />
+  </div>
+</template>
+ 
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import WellcomeJumbo from "./WellcomeJumbo.vue";
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    WellcomeJumbo
+  }
+};
+</script>
