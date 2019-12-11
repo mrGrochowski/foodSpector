941916c191cece232f515d7000813061460df1bc
Unknown <mateusz.grochowski1992@gmail.com>
2019-12-12 00:19:02 +0100

    tweak display approach

--- src/views/Home/WellcomeJumbo.vue
+++ src/views/Home/WellcomeJumbo.vue
@@ -22,25 +22,31 @@ export default {
   display: inline-flex;
   flex-direction: column;
   width: 100%;
-  min-height: 42vh;
+  //min-height: 42vh;
+  //min-height: 325px;
   background-color: #fff;
   position: relative;
   z-index: 0;
   &:before {
     content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0px;
+    // position: absolute;
+    // left: 0;
+    // top: 0;
+    // right: 0;
+    // bottom: 0px;
+
+    height: 220px;
+    margin-left: -15px;
+    margin-right: -15px;
     background-image: url("../../assets/images/laura-mitulla-52-C-unsplash.jpg");
     background-repeat: no-repeat;
+    background-position: 20%;
     background-size: cover;
     mask-image: linear-gradient(
       180deg,
       rgba(0, 0, 0, 1) 20%,
-      rgba(0, 0, 0, 1) 80%,
-      rgba(0, 0, 0, 0) 95%
+      rgba(0, 0, 0, 1) 70%,
+      rgba(0, 0, 0, 0) 100%
     );
     mask-repeat: space;
     z-index: 0;
@@ -48,16 +54,19 @@ export default {
 }
 .wellcome-jumbo__header {
   font-weight: 700;
-  line-height: 1.25;
+  line-height: 1.275;
   //justify-self: flex-end;
   z-index: 1;
   color: #4f4f4f;
   font-size: 18px;
-  margin-top: auto;
+  margin-top: -30px;
 }
 
 .wellcome-jumbo__description {
   font-weight: 500;
-  font-size: 16px;
+  font-size: 14px;
+  padding-top: 10px;
+
+  color: #646464;
 }
 </style>
